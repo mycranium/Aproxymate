7e4308a1c1e175fb751e1f074040a968ece8b0a5
Mike Rainey <7647165+mycranium@users.noreply.github.com>
2021-09-26 14:55:10 -0400

    Removed extraneous comments and pseudo-committed

--- Scripts/Aproxymate.jsx
+++ Scripts/Aproxymate.jsx
@@ -1,5 +1,5 @@
 {
-    app.beginUndoGroup("Comments"); // Create an undo groupvar myCollection = app.project.items;
+    app.beginUndoGroup("Replace Source with Proxy"); // Create an undo groupvar myCollection = app.project.items;
     if (myCollection.length > 0) {
         for (i = 1; i <= myCollection.length; i++) {
             if (myCollection[i].proxySource) {
@@ -16,29 +16,4 @@
     // close the undo group
     app.endUndoGroup();
 }
-
-
-    //var srcFile = new ImportOptions(testFile);
-    //app.project.importFile(srcFile);
-    //app.project.importFile(new ImportOptions(File(“sample.psd”))
-
-//alert(myCollection.length);
-// Project overhead
-// get project items into collection
-// iterate collection, get items into array if hasProxy
-// if items,
-// Prompt 1- delete originals Y/N
-// Prompt 2 - Ignore items with disabled proxies
-// item get type deletable (if type = footage false else true)
-// get proxysource
-// item proxyActive?
-// if !Prompt 2 
-    // source.replace(proxysource)
-    // if item.deletable && Prompt 1
-        // source.delete
-// else
-    // if item.active
-        // source.replace(proxysource)
-        // if item.deletable && Prompt 1
-            // source.delete
 
